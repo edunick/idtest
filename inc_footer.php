9c0df8099ca24b0fb01f64039a53c916da4a7ad5
Eduardo <edunick@gmail.com>
2023-06-13 15:25:46 -0300

    Update inc_footer.php

--- inc_footer.php
+++ inc_footer.php
@@ -4,6 +4,6 @@
 <p><!--<span class="first">Esponsor: </span><a class="block filamentgroup" href="http://www.test.com/"><span>esponsor test</span></a>-->
 <span class="first" style="float: right; padding-right: 12px;">© 2011 <a
 	href="http://sic.com/">SIC</a> y <a href="http://vortice">VORTICE</a>.</span>
-</p>
+</p>asdasd
 </div>
 </div>
